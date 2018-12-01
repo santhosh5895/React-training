0945f5737022fbabc20b4764feb0cd8e49c35e74
Santhosh <santhosh5895@gmail.com>
2018-12-01 17:44:24 +0530

    Added header component class

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render(){
+        return(
+            <header>
+                <h3>Header</h3>
+                <input/>
+            </header>
+        )
+    }
+};
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/header';
 
 var First = () =>{
-    return <h1>This is your first React app</h1>
+    return (
+    <div>
+        <Header/>
+        <h1>This is your first React app</h1>
+    </div>
+    )
 }
 
 ReactDOM.render(<First/>,document.querySelector("#root"))
