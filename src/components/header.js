5322963b455e4b2d76cd0e530b124f2ce78199f9
Santhosh <santhosh5895@gmail.com>
2018-12-01 19:36:42 +0530

    Use of onClick and onChange

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 
 class Header extends Component {
+    printType(event){
+        console.log(event.target.value);
+    }
     render(){
         return(
             <header>
-                <h3 className="logo">Header</h3>
-                <input/>
+                <h3 className="logo" onClick={()=>console.log("Logo clicked!")}>Header</h3>
+                <input onChange={this.printType}/>
             </header>
         )
     }
