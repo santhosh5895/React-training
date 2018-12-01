2cfa83f6fef65471dc414666c660257c7c4ff7ad
Santhosh <santhosh5895@gmail.com>
2018-12-01 19:14:04 +0530

    Added basic style

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render(){
         return(
             <header>
-                <h3>Header</h3>
+                <h3 className="logo">Header</h3>
                 <input/>
             </header>
         )
