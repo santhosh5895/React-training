a4b94b0ef25caf555e15c4f1416000725750863f
Santhosh <santhosh5895@gmail.com>
2018-12-01 21:17:56 +0530

    Using state in header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            Title : "This is the title:",
+            Value : "default"
+        }
+    }
     printType(event){
-        console.log(event.target.value);
+        this.setState({Value: event.target.value});
     }
     render(){
         return(
             <header>
                 <h3 className="logo" onClick={()=>console.log("Logo clicked!")}>Header</h3>
-                <input onChange={this.printType}/>
+                <input onChange={this.printType.bind(this)}/>
+                <div>{this.state.Title}  {this.state.Value}</div>
             </header>
         )
     }
